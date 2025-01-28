26f3eb61ecf6c894b5241ad2bc145e3ed991223a
patel-udit-patel <udit0428t@gmail.com>
2025-01-28 22:44:20 +0530

    First commit of week3

---
+++ First.java
@@ -0,0 +1,7 @@
+import javax.crypto.spec.PSource;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("This is the first java file of week03");
+    }
+}
